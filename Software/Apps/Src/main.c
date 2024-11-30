e83c6e69e743b6b23a4b72f7c862d01b71b5cdf5
Ravi Shah <ravidhruvishshah@gmail.com>
2024-11-30 13:32:06 -0600

    enum for crc validation

--- Software/Apps/Src/main.c
+++ Software/Apps/Src/main.c
@@ -25,6 +25,14 @@ uint16_t checksum_rh;
 const uint16_t crc_poly = 0x31;
 const uint16_t crc_initial = 0xFF;
 
+enum SHT45_CRC_Result
+{
+    VALID = 0,
+    TEMP_VALID = 1,
+    RH_VALID = 2,
+    INVALID = 3
+};
+
 StaticTask_t task_buffer;
 StackType_t taskStack[configMINIMAL_STACK_SIZE];
 
@@ -32,11 +40,12 @@ StackType_t taskStack[configMINIMAL_STACK_SIZE];
 void SystemClock_Config(void);
 static void MX_I2C1_Init(void);
 static void MX_CAN_Init(void);
-// static void MX_CRC_Init(void);
 
 static void task(void *pvParameters);
 static void error_handler(void);
 static void success_handler(void);
+static enum SHT45_CRC_Result SHT45_CRC(uint8_t RX_Buffer[6]);
+// TODO: enum for return vals of CRC calculations
 
 int main(void)
 {
@@ -47,7 +56,6 @@ int main(void)
     /* Initialize all configured peripherals */
     MX_I2C1_Init();
     MX_CAN_Init();
-    //  MX_CRC_Init();
 
     xTaskCreateStatic(
         task,
@@ -64,21 +72,6 @@ int main(void)
 
     return 0;
 }
-
-/*static void MX_CRC_Init(void)
-{
-    hcrc.Instance = CRC;
-    hcrc.Init.DefaultPolynomialUse = DEFAULT_POLYNOMIAL_ENABLE;
-    hcrc.Init.DefaultInitValueUse = DEFAULT_INIT_VALUE_ENABLE;
-    hcrc.Init.InputDataInversionMode = CRC_INPUTDATA_INVERSION_BYTE;
-    hcrc.Init.OutputDataInversionMode = CRC_OUTPUTDATA_INVERSION_ENABLE;
-    hcrc.InputDataFormat = CRC_INPUTDATA_FORMAT_BYTES;
-
-    if (HAL_CRC_Init(&hcrc) != HAL_OK)
-    {
-        error_handler();
-    }
-}*/
 
 // CAN initialization function
 static void MX_CAN_Init(void)
@@ -165,6 +158,121 @@ static void error_handler(void)
     }
 }
 
+// calculate and verify CRC-8 for the SHT45 humidity sensor
+static enum SHT45_CRC_Result SHT45_CRC(uint8_t RX_Buffer[6])
+{
+    checksum_temp = 0;
+    checksum_rh = 0;
+    uint8_t computed_temp_crc;
+    uint8_t computed_rh_crc;
+    enum SHT45_CRC_Result SHT45_valid = INVALID; // set result to INVALID by default
+
+    // use checksum to verify data is received correctly - important when we use the breakout boards as that may introduce noise on i2c bus
+    checksum_temp = RX_Buffer[2];
+
+    // TEST: manually set known values... checksum of 0xBEEF = 0x92 (according to SHT45 datasheet)
+    // RX_Buffer[0] = 0xBE;
+    // RX_Buffer[1] = 0xEF;
+    // checksum_temp = 0x92;
+
+    // manually implemented using algo at https://www.st.com/resource/en/application_note/an4187-using-the-crc-peripheral-on-stm32-microcontrollers-stmicroelectronics.pdf
+    // sequentially - do the crc for first byte first and then use that val as input data for the crc for second byte and should give you proper value, verify this with what we actually recieve from the sensor
+
+    computed_temp_crc = crc_initial ^ RX_Buffer[0]; // 1st step of CRC for 1st temp byte
+
+    // calc crc for first byte
+    for (uint8_t bindex = 0; bindex < sizeof(RX_Buffer[0]) * 8; bindex++)
+    {
+        uint8_t MSB = (int8_t)computed_temp_crc < 0; // get MSB of CRC
+
+        if (MSB == 1)
+        {
+            computed_temp_crc = (computed_temp_crc << 1) ^ crc_poly; // shift one bit left, XOR with poly
+        }
+        else
+        {
+            computed_temp_crc = computed_temp_crc << 1; // shift one bit left
+        }
+    }
+
+    // sequential calc for second byte (using first byte crc as initial crc)
+    computed_temp_crc = computed_temp_crc ^ RX_Buffer[1]; // 1st step of CRC for 2nd temp byte
+
+    for (uint8_t bindex = 0; bindex < sizeof(RX_Buffer[1]) * 8; bindex++)
+    {
+        uint8_t MSB = (int8_t)computed_temp_crc < 0; // get MSB of CRC
+
+        if (MSB == 1)
+        {
+            computed_temp_crc = (computed_temp_crc << 1) ^ crc_poly; // shift one bit left, XOR with poly
+        }
+        else
+        {
+            computed_temp_crc = computed_temp_crc << 1; // shift one bit left
+        }
+    }
+
+    // same calulcation for rh bytes
+    checksum_rh = RX_Buffer[5];
+
+    computed_rh_crc = crc_initial ^ RX_Buffer[3]; // 1st step of CRC for 1st rh byte
+
+    // calc crc for first byte
+    for (uint8_t bindex = 0; bindex < sizeof(RX_Buffer[3]) * 8; bindex++)
+    {
+        uint8_t MSB = (int8_t)computed_rh_crc < 0; // get MSB of CRC
+
+        if (MSB == 1)
+        {
+            computed_rh_crc = (computed_rh_crc << 1) ^ crc_poly; // shift one bit left, XOR with poly
+        }
+        else
+        {
+            computed_rh_crc = computed_rh_crc << 1; // shift one bit left
+        }
+    }
+
+    // sequential calc for second byte (using first byte crc as initial crc)
+    computed_rh_crc = computed_rh_crc ^ RX_Buffer[4]; // 1st step of CRC for 2nd rh byte
+
+    for (uint8_t bindex = 0; bindex < sizeof(RX_Buffer[4]) * 8; bindex++)
+    {
+        uint8_t MSB = (int8_t)computed_rh_crc < 0; // get MSB of CRC
+
+        if (MSB == 1)
+        {
+            computed_rh_crc = (computed_rh_crc << 1) ^ crc_poly; // shift one bit left, XOR with poly
+        }
+        else
+        {
+            computed_rh_crc = computed_rh_crc << 1; // shift one bit left
+        }
+    }
+
+    // check if computed crc bytes match received bytes
+    if (computed_temp_crc == checksum_temp)
+    {
+        if (computed_rh_crc == checksum_rh)
+        {
+            SHT45_valid = VALID;
+        }
+        else
+        {
+            SHT45_valid = TEMP_VALID;
+        }
+    }
+    else if (computed_rh_crc == checksum_rh)
+    {
+        SHT45_valid = RH_VALID;
+    }
+    else
+    {
+        SHT45_valid = INVALID;
+    }
+
+    return SHT45_valid;
+}
+
 // blinks LED upon successful CAN transmission
 static void success_handler(void)
 {
@@ -193,10 +301,6 @@ static void task(void *pvParameters)
         temp_ticks = 0;
         rh_ticks = 0;
         uint8_t RX_Buffer[6] = {0}; // Data received from humidity sensor
-        checksum_temp = 0;
-        checksum_rh = 0;
-        uint8_t computed_temp_crc;
-        //uint16_t computed_rh_crc;
 
         HAL_I2C_Master_Transmit(&hi2c1, (uint16_t)(0x44 << 1), TX_Buffer, sizeof TX_Buffer, 1000); // Sending in Blocking mode
         HAL_Delay(10);                                                                             // wait for 0.01 seconds (datasheet value) for response from sensor
@@ -210,60 +314,36 @@ static void task(void *pvParameters)
         rh_ticks = (RX_Buffer[3] * 256) + RX_Buffer[4];
         rh_percentRH = (-6 + (125 * rh_ticks / 65535));
 
-        // TODO: if percent rh is < 0, set to 0. if percent rh is > 100, set to 100
-
-        // use checksum to verify data is received correctly - important when we use the breakout boards as that may introduce noise on i2c bus
-        checksum_temp = RX_Buffer[2];
-
-        // TEST: manually set known values...
-        RX_Buffer[0] = 0xBE;
-        RX_Buffer[1] = 0xEF;
-
-        // manually implemented using algo at https://www.st.com/resource/en/application_note/an4187-using-the-crc-peripheral-on-stm32-microcontrollers-stmicroelectronics.pdf
-        // sequentially - do the crc for first byte first and then use that val as input data for the crc for second byte and should give you proper value, verify this with what we actually recieve from the sensor
-
-        computed_temp_crc = crc_initial ^ RX_Buffer[0]; // 1st step of CRC for 1st temp bit
-
-        // calc crc for first byte
-        for (uint8_t bindex = 0; bindex < sizeof(RX_Buffer[0]) * 8; bindex++)
+        // if percent rh is < 0, set to 0. if percent rh is > 100, set to 100 (expected behavior from SHT45 datasheet)
+        if (rh_percentRH < 0)
         {
-            uint8_t MSB = (int8_t)computed_temp_crc < 0; // get MSB of CRC
-
-            if (MSB == 1)
-            {
-                computed_temp_crc = (computed_temp_crc << 1) ^ crc_poly; // shift one bit left, XOR with poly
-            }
-            else
-            {
-                computed_temp_crc = computed_temp_crc << 1; // shift one bit left
-            }
+            rh_percentRH = 0;
+        }
+        else if (rh_percentRH > 100)
+        {
+            rh_percentRH = 100;
         }
 
-        computed_temp_crc = computed_temp_crc ^ RX_Buffer[1]; // 1st step of CRC for 2nd temp bit
-
-        // sequential calc for second byte
-        for (uint8_t bindex = 0; bindex < sizeof(RX_Buffer[1]) * 8; bindex++)
+        // handle CRC result
+        enum SHT45_CRC_Result SHT45_valid = SHT45_CRC(RX_Buffer); // calculate and verify CRC - returns value according to enum above
+        // any invalid values are sent as zeros as per DataAcq specs
+        if (SHT45_valid == TEMP_VALID)
         {
-            uint8_t MSB = (int8_t)computed_temp_crc < 0; // get MSB of CRC
-
-            if (MSB == 1)
-            {
-                computed_temp_crc = (computed_temp_crc << 1) ^ crc_poly; // shift one bit left, XOR with poly
-            }
-            else
-            {
-                computed_temp_crc = computed_temp_crc << 1; // shift one bit left
-            }
+            rh_percentRH = 0;
         }
-
-        // TODO: check if computed crc == received crc
-
-        // TODO: same thing for rh
-        checksum_rh = RX_Buffer[5];
+        else if (SHT45_valid == RH_VALID)
+        {
+            temp_degC = 0;
+        }
+        else if (SHT45_valid == INVALID)
+        {
+            temp_degC = 0;
+            rh_percentRH = 0;
+        }
 
         // create CAN payload to send data on DataAcq CAN bus
         CAN_TxHeaderTypeDef tx_header = {0};
-        tx_header.StdId = 0x200;
+        tx_header.StdId = 0x200; // base address of environment board (starts at x200)
         tx_header.RTR = CAN_RTR_DATA;
         tx_header.IDE = CAN_ID_STD;
         tx_header.DLC = 2;
