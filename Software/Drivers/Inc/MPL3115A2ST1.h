b4f319dbde2fe455e3c56b8f4617bec5562f7c76
Ravi Shah <ravidhruvishshah@gmail.com>
2025-04-16 16:31:04 -0500

    moved drivers to drivers

---
+++ Software/Drivers/Inc/MPL3115A2ST1.h
@@ -0,0 +1,7 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MPL3115A2ST1_H
+#define __MPL3115A2ST1_H
+
+#define MPL3115A2ST1_I2C_ADDRESS 0xC0 // default MPL3115A2ST1 base address is 0xC0
+
+#endif
